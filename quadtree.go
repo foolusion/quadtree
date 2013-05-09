eeddfd32664f9da06aecc7f9629c515de2475b18
Andrew O'Neill <foolusion@gmail.com>
2013-05-08 22:23:04 -0400

    Basic structures and mock functions

---
+++ quadtree.go
@@ -0,0 +1,33 @@
+package quadtree
+
+// XY is a simple coordinate structure for points and vectors
+type XY struct {
+	X float64
+	Y float64
+}
+
+// creates a new point and returns address
+func NewXY(x, y float64) *XY {
+	return &XY{x, y}
+}
+
+// Axis-Aligned bounding box structure with center and half dimension
+type AABB struct {
+	Center XY
+	HalfDimension XY
+}
+
+// creates a new aabb and returns its address
+func NewAABB(center, halfDimension XY) *AABB {
+	return &AABB{center, halfDimension}
+}
+
+// Contains Point returns true when the AABB contains the point given
+func (aabb *AABB) ContainsPoint(p XY) bool {
+	return false
+}
+
+// Intersects AABB returns true when the AABB intersects another AABB
+func (aabb *AABB) IntersectsAABB(other AABB) bool {
+	return false
+}
